24746e2ff6bef406e4d98e58031c95e1ce7b46ec
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-12-14 21:55:37 +0200

    ✨: add Food class

---
+++ Agar.io/Assets/Scripts/Model/Food.cs
@@ -0,0 +1,12 @@
+using System.Numerics;
+
+namespace Agario.Model
+{
+    public class Food
+    {
+        public Vector2 Position { get; set; }
+        public float Size { get; set; }
+
+        public void Die() { }
+    }
+}
