158823826f975d120c98359e958b008ec9e330b6
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-12-14 21:49:59 +0200

    ✨: add collision detection

---
+++ Agar.io/Assets/Scripts/Model/CollisionDetection.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace Agario.Model
+{
+    public static class CollisionDetection
+    {
+        public static bool AreColliding(Vector2 firstPosition,
+            Vector2 secondPosition, float firstRadius, float secondRadius)
+        {
+            var radius = firstRadius + secondRadius;
+
+            var deltaX = firstPosition.X - secondPosition.X;
+            var deltaY = firstPosition.Y - secondPosition.Y;
+
+            return deltaX * deltaX + deltaY * deltaY <= radius * radius;
+        }
+    }
+}
