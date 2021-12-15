2ef4a9aae3ebf86f2438df8ef575761eceafe6cd
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-12-15 14:53:09 +0200

    ✨: add Move and GetTotalRadius

--- Agar.io/Assets/Scripts/Model/Player.cs
+++ Agar.io/Assets/Scripts/Model/Player.cs
@@ -1,12 +1,31 @@
 using System.Collections.Generic;
+using System.Numerics;
 
 namespace Agario.Model
 {
     public class Player
     {
         public string Name { get; set; }
-        public List<Blob> Blobs {get; set;}
+        public List<Blob> Blobs { get; set; }
 
         public void Die() { }
+        public void Move(Vector2 direction)
+        {
+            foreach (Blob blob in Blobs)
+            {
+                blob.Move(direction);
+            }
+        }
+        public float GetTotalRadius()
+        {
+            float totalRadius = 0f;
+
+            foreach (Blob blob in Blobs)
+            {
+                totalRadius += blob.Radius;
+            }
+
+            return totalRadius;
+        }
     }
 }
