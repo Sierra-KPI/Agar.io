21ec691b041312dde28f8e6a5dcd89f63cca374f
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-12-15 14:52:40 +0200

    📝: add To-Do in [Blob.cs]

--- Agar.io/Assets/Scripts/Model/Blob.cs
+++ Agar.io/Assets/Scripts/Model/Blob.cs
@@ -7,9 +7,26 @@ namespace Agario.Model
         public Vector2 Position { get; set; }
         public float Radius { get; set; }
 
-        public void Die() { }
-        public void Move() { }
-        public void Combine() { }
-        public void Split() { }
+        public void Die()
+        {
+            // die(
+        }
+
+        public void Move(Vector2 direction)
+        {
+            Position += direction;
+            //Position += move towards the center
+        }
+
+        public void Combine()
+        {
+            // if it is colliding with another blob of this player
+        }
+
+        public void Split()
+        {
+            // on spacebar reduce its radius in half and add some
+            // force towards mouse position
+        }
     }
 }
