b208521761d2acd2013e2f3cf52e8c62707d79a1
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-12-17 11:42:22 +0200

    :sparkles:: add EntityFactory

---
+++ Agar.io/Assets/Scripts/Model/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System.Numerics;
+
+namespace Agario.Model
+{
+
+    public enum EntityType
+    {
+        Food,
+        Blob
+    }
+
+
+    public abstract class Entity
+    {
+        public Vector2 Position { get; set; }
+        public float Radius { get; set; }
+        public EntityType EntityType { get; set; }
+    }
+}
