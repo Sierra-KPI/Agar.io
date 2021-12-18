ac631b905cb331e6e85fdb9057f0d77b2057c69c
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-12-18 15:32:26 +0200

    ♻️: separate one class into two

--- Agar.io/Assets/Scripts/Model/Entity.cs
+++ Agar.io/Assets/Scripts/Model/Entity.cs
@@ -2,18 +2,11 @@
 
 namespace Agario.Model
 {
-
-    public enum EntityType
-    {
-        Food,
-        Blob
-    }
-
-
     public abstract class Entity
     {
         public Vector2 Position { get; set; }
         public float Radius { get; set; }
         public EntityType EntityType { get; set; }
+        public bool IsDead { get; set; }
     }
 }
