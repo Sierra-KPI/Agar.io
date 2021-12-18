38abff607286f300f8f0cc1469c5f680d3403e10
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-12-18 13:11:51 +0200

    ♻️: separate one class into two

---
+++ Agar.io/Assets/Scripts/View/EntityObject.cs
@@ -0,0 +1,20 @@
+﻿using Agario.Model;
+using UnityEngine;
+
+namespace Agario.View
+{
+    public class EntityObject
+    {
+        public EntityType EntityType;
+        public GameObject Prefab;
+        public int Number;
+
+        public EntityObject(EntityType entityType, 
+            GameObject prefab, int number)
+        {
+            EntityType = entityType;
+            Prefab = prefab;
+            Number = number;
+        }
+    }
+}
