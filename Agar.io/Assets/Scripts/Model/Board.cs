bded4d9d8a064434c87ad685c136afbf932d2e23
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-12-22 12:04:55 +0200

    :sparkles:: add classes Board and Chunk

---
+++ Agar.io/Assets/Scripts/Model/Board.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agario.Model
+{
+    class Board
+    {
+
+        public Chunk[] Chunks;
+        public int Width = 100;
+        
+
+        public Board()
+        {
+
+        }
+
+        public void CreateChunks()
+        {
+
+        }
+
+        public void GetConnectedChunks(int chunkId)
+        {
+
+        }
+
+        public void GetChunkByPosition(Vector2 position)
+        {
+
+        }
+
+
+    }
+}
---
+++ Agar.io/Assets/Scripts/Model/Chunk.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agario.Model
+{
+    class Chunk
+    {
+        public int Id;
+        public int Width = 5;
+        public List<Entity> Entities = new();
+
+
+
+
+    }
+}
