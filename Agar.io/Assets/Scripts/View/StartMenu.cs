a7297e2464664b560d5f6de256fea6892822eeee
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-12-23 21:24:58 +0200

    ✨: add static variable to remember username

--- Agar.io/Assets/Scripts/View/StartMenu.cs
+++ Agar.io/Assets/Scripts/View/StartMenu.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Agario.UnityView;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -28,7 +29,9 @@ public class StartMenu : MonoBehaviour
     {
         var username = _inputField.text;
         Debug.Log("Connect: " + username);
-        // add static field to view for username
+
+        View.s_username = username;
+
         SceneManager.LoadScene(_mainSceneName);
     }
 
