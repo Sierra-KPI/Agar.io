96e9da9d4ef90b802fb84e06f8643f31b3c3b252
Anastasiya Trembach <nastia.trembach@gmail.com>
2021-12-24 22:07:34 +0200

    :sparkles:: add logic for table

--- Agar.io/Assets/Scripts/View/EndMenu.cs
+++ Agar.io/Assets/Scripts/View/EndMenu.cs
@@ -9,10 +9,21 @@ public class EndMenu : MonoBehaviour
     private readonly string _mainSceneName = "MainScene";
     private Button _quitButton;
     private Button _startButton;
-
+    [SerializeField]
+    private GameObject _rowPrefab;
 
     void Start()
     {
+        var table = GameObject.Find("Table");
+
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject row = Instantiate(_rowPrefab, table.transform);
+            Text[] cells = row.GetComponentsInChildren<Text>();
+            cells[0].text = (i + 1).ToString();
+            cells[1].text = "Username" + i;
+            cells[2].text = ((5-i)*6.829).ToString("f0");
+        }
 
         _startButton = GameObject.Find("StartButton").GetComponent<Button>();
         _startButton.onClick.AddListener(delegate { OnStartButtonClick(); });
