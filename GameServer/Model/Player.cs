5bfeb6dd08236360c6d71113268b1831226ceeea
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-12-25 12:16:41 +0200

    ♻️: change static startRadius to const

--- GameServer/Model/Player.cs
+++ GameServer/Model/Player.cs
@@ -4,13 +4,13 @@ namespace Agario.Model
 {
     public class Player : Entity
     {
-        public static float startRadius = 3;
-        public string Name { get; set; }
+        private const float StartRadius = 3;
+        public string Name { get; set; } = "Default name";
 
         public Player(Vector2 position)
         {
             EntityType = EntityType.Player;
-            Radius = startRadius;
+            Radius = StartRadius;
             Position = position;
             ChunkId = Board.GetChunkIdByPosition(Position);
         }
