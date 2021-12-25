ce558dba02889501212fb60d81e0dc82a6a9706a
Valentyn Dominskyi <Valentyn2202@gmail.com>
2021-12-25 22:01:20 +0200

    ♻️: change static Width to const

--- GameServer/Model/Chunk.cs
+++ GameServer/Model/Chunk.cs
@@ -5,14 +5,12 @@ namespace Agario.Model
     public class Chunk
     {
         public int Id;
-        public static int Width = 20;
+        public const int Width = 20;
         public List<Entity> Entities = new();
 
         public Chunk(int id)
         {
             Id = id;
         }
-
-
     }
 }
